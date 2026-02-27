6bcbcfa9991d139b963ffe334ed2d440f15665a6
andre4ik3 <andre4ik3@fastmail.com>
2026-02-27 08:41:18 +0000

    show builds

--- src/app.rs
+++ src/app.rs
@@ -5,13 +5,16 @@ use futures::{FutureExt, StreamExt};
 use std::time::Duration;
 use tokio::{sync::mpsc, time};
 
+use ratatui::text::Line;
+use ratatui::widgets::{List, ListItem, ListState};
 use ratatui::{
+    layout::Alignment, style::{Style, Stylize}
+    ,
+    widgets::{Block, BorderType, StatefulWidget, Widget},
     DefaultTerminal,
-    buffer::Buffer,
-    layout::{Alignment, Rect},
-    style::{Color, Stylize},
-    widgets::{Block, BorderType, Paragraph, Widget},
+    Frame,
 };
+use ratatui::prelude::Color;
 
 #[derive(Debug)]
 pub enum AppEvent {
@@ -28,11 +31,17 @@ pub enum Event {
 #[derive(Debug)]
 pub struct App {
     running: bool,
+
+    // event plumbing
     sender: mpsc::UnboundedSender<Event>,
     receiver: mpsc::UnboundedReceiver<Event>,
 
+    // getting builds
     pub refresh_interval: Duration,
     pub active_builds: Vec<ps::Build>,
+
+    // stuff
+    pub list_state: ListState,
 }
 
 impl App {
@@ -44,11 +53,8 @@ impl App {
             receiver,
             refresh_interval: Duration::from_secs(5),
             active_builds: Vec::new(),
+            list_state: ListState::default(),
         }
-    }
-
-    fn send(&self, event: AppEvent) {
-        _ = self.sender.send(Event::App(event));
     }
 
     /// Run the application's main loop.
@@ -73,7 +79,7 @@ impl App {
             .send(Event::App(AppEvent::Refresh(ps::get().await)));
 
         while self.running {
-            terminal.draw(|frame| frame.render_widget(&self, frame.area()))?;
+            terminal.draw(|frame| self.render(frame))?;
             match self
                 .receiver
                 .recv()
@@ -97,8 +103,8 @@ impl App {
         Ok(())
     }
 
-    /// Handles the key events and updates the state of [`App`].
-    pub fn handle_key_events(&mut self, key_event: KeyEvent) -> anyhow::Result<()> {
+    /// Handles terminal key events.
+    fn handle_key_events(&mut self, key_event: KeyEvent) -> anyhow::Result<()> {
         match key_event.code {
             // refresh interval
             KeyCode::Char('-') => {
@@ -116,16 +122,22 @@ impl App {
                     .saturating_add(Duration::from_millis(100));
             }
 
+            // active builds table
+            KeyCode::Up => self.list_state.select_previous(),
+            KeyCode::Down => self.list_state.select_next(),
+            KeyCode::Esc => self.list_state.select(None),
+
             // quitting
-            KeyCode::Esc | KeyCode::Char('q') => _ = self.send(AppEvent::Quit),
+            KeyCode::Char('q') => _ = self.sender.send(Event::App(AppEvent::Quit)),
             KeyCode::Char('c' | 'C') if key_event.modifiers == KeyModifiers::CONTROL => {
-                self.send(AppEvent::Quit)
+                _ = self.sender.send(Event::App(AppEvent::Quit));
             }
             _ => {}
         }
         Ok(())
     }
 
+    /// Processes a received `nix ps` output and schedules the next one to run.
     fn refresh(&mut self, output: anyhow::Result<ps::Output>) {
         if let Ok(builds) = output {
             // TODO: handle errors
@@ -150,30 +162,42 @@ impl App {
             }
         });
     }
+
+    fn render(&mut self, frame: &mut Frame) {
+        let block = Block::bordered()
+            .title_top(
+                Line::from(vec![
+                    "-".red(),
+                    format!(" {}ms ", self.refresh_interval.as_millis()).white(),
+                    "+".red(),
+                ])
+                .alignment(Alignment::Right)
+            )
+            .title_bottom(Line::from(vec![
+                "↑".red(),
+                " select ".white(),
+                "↓".red(),
+            ]))
+            .border_type(BorderType::Rounded)
+            .border_style(Style::new().black());
+
+        let list = List::new(&self.active_builds)
+            .block(block)
+            .highlight_style(Style::new().bg(Color::Rgb(19, 57, 117)))
+            .repeat_highlight_symbol(true);
+
+        frame.render_stateful_widget(list, frame.area(), &mut self.list_state);
+    }
 }
 
-impl Widget for &App {
-    fn render(self, area: Rect, buf: &mut Buffer) {
-        let block = Block::bordered()
-            .title("ntop")
-            .title_alignment(Alignment::Center)
-            .border_type(BorderType::Rounded);
-
-        let text = format!(
-            "This is a tui template.\n\
-                Press `Esc`, `Ctrl-C` or `q` to stop running.\n\
-                Press left and right to increment and decrement the counter respectively.\n\
-                Refresh interval: {}ms, Active builds: {}",
-            self.refresh_interval.as_millis(),
-            self.active_builds.len()
-        );
-
-        let paragraph = Paragraph::new(text)
-            .block(block)
-            .fg(Color::Cyan)
-            .bg(Color::Black)
-            .centered();
-
-        paragraph.render(area, buf);
+impl<'a> Into<ListItem<'a>> for &ps::Build {
+    fn into(self) -> ListItem<'a> {
+        // drop hash prefix and .drv suffix
+        let name = &self.derivation[33..self.derivation.len() - 4];
+        if let Some((pname, version)) = name.rsplit_once('-') {
+            ListItem::new(format!("{} ({})", pname, version))
+        } else {
+            ListItem::new(format!("{}", name))
+        }
     }
 }
